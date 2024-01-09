06c6237f481727836461dd4f034830ef8666862b
Jana May <jana.ricarda.may@gmail.com>
2024-01-09 10:35:24 +0100

    solve challenge

--- js/index.js
+++ js/index.js
@@ -1,5 +1,14 @@
 console.clear();
 
+const inputElement = document.querySelector('[data-js="first-input"]');
+const button = document.querySelector('[data-js="button-uppercase"]');
+
+button.addEventListener("click", () => {
+  const oldValue = inputElement.value;
+  const newValue = oldValue.toUpperCase();
+  inputElement.value = newValue;
+});
+
 /*
 Convert the input value to uppercase when clicking the button
 
